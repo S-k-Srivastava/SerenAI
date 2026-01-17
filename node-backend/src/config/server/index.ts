9df2fedc3f8c8c64218a5b2df49721eae9a0a3a0
S-K-Srivastava <9s8k7s@gmail.com>
2026-01-17 20:20:13 +0530

    Incerase API and emebdding docker container limit to handle documents upto 50MB

--- node-backend/src/config/server/index.ts
+++ node-backend/src/config/server/index.ts
@@ -60,8 +60,8 @@ export const createServer = (): express.Application => {
     app.use(limiter);
   }
 
-  app.use(express.json({ limit: "10mb" }));
-  app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
   app.use(requestLogger);
 
